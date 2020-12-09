f9308d86942f1c30a1c9843c44402dcfe50bb763
Fabrice ASSEMIEN <asskroah@gmail.com>
2020-12-09 19:33:38 +0000

    first commit

---
+++ src/main/java/com/veone/greeterlibrary/GreetingConfig.java
@@ -0,0 +1,7 @@
+package com.veone.greeterlibrary;
+
+import java.util.Properties;
+
+public class GreetingConfig extends Properties {
+    private static final long serialVersionUID = 5662570853707247891L;
+}
---
+++ src/test/java/com/veone/greeterlibrary/GreeterLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veone.greeterlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
